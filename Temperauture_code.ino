dd16e6f520f5b007e41a7004373f4c96a5c9cfb9
efyrs <122615873+efyrs@users.noreply.github.com>
2023-02-21 12:43:27 +0000

    Add files via upload

---
+++ Temperauture_code.ino
@@ -0,0 +1,126 @@
+//****************************************//
+//* ESP32 MQTT EEEBot Template           *//
+//* Modified from Rui Santos'            *//
+//* https://randomnerdtutorials.com      *//
+//*                                      *//
+//* ESP32 Code                           *//
+//*                                      *//
+//* UoN 2022 - ND                        *//
+//****************************************//
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// Add your required sensor/component libraries here
+// --
+#include <MPU6050_tockn.h>
+MPU6050 mpu6050(Wire);
+// --
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "corulero";                      //CHANGE ME
+const char* password = "rulecoro";              //CHANGE ME     
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";                      
+const char* mqtt_server = "192.168.2.1";          //CHANGE ME
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Add your subscribed topics here i.e. statements to control GPIOs with MQTT
+  // --
+  
+  // --
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      
+      // Add your subscribe topics here
+      // --
+      
+      // --
+         
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Add your own code here i.e. sensor measurements, publish topics & subscribe topics for GPIO control
+    // --
+    mpu6050.update();
+  float temp = mpu6050.getTemp();
+  char tempString[8];
+  dtostrf(temp,1,2,tempString);
+  client.publish("esp32/temp",tempString);
+  Serial.print("\ntemp: ");
+  Serial.print(temp);
+    // --
+  }
+}
